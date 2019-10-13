b2f770562a7b25bf868b395fa002361992670af5
hermes <hermes_48@hotmail.com>
2019-10-14 00:21:54 +0200

    added swap() refactor & Wip SearchPivot

--- src/main/java/com/ing/mb/mwchapter/LexicographicalOrderMvpImplUW49AT.java
+++ src/main/java/com/ing/mb/mwchapter/LexicographicalOrderMvpImplUW49AT.java
@@ -2,6 +2,7 @@ package com.ing.mb.mwchapter;
 
 import java.util.Arrays;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 public class LexicographicalOrderMvpImplUW49AT implements LexicographicalOrder {
 
@@ -12,17 +13,26 @@ public class LexicographicalOrderMvpImplUW49AT implements LexicographicalOrder {
 
 
     private Optional<String> calcNextLargerWord(String word) {
+//        Integer pivot = calcPivot(getArrayFromString(word)).orElse(null);
+//        if (pivot.equals(null)) return Optional.empty();
+
+
         String next = getStringFromASCIIArray(nextPermutation(getArrayFromString(word)));
-        return (next.equals(word))? Optional.empty():Optional.of(next);
+        return (next.equals(word)) ? Optional.empty() : Optional.of(next);
     }
 
     public static int[] nextPermutation(int[] array) {
+
+
         // Find non-increasing suffix
         int i = array.length - 1;
         while (i > 0 && array[i - 1] >= array[i])
             i--;
         if (i <= 0)
             return array;
+
+//        Integer i = calcPivotStream(array);
+
         // Find successor to pivot
         int j = array.length - 1;
         while (array[j] <= array[i - 1])
@@ -34,13 +44,18 @@ public class LexicographicalOrderMvpImplUW49AT implements LexicographicalOrder {
         // Reverse suffix
         j = array.length - 1;
         while (i < j) {
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
+            swap(array, i, j);
             i++;
             j--;
         }
         return array;
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp;
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
 
     private static int[] getArrayFromString(String word) {
@@ -54,6 +69,15 @@ public class LexicographicalOrderMvpImplUW49AT implements LexicographicalOrder {
                 .map(k -> (char) k.intValue())
                 .map(Object::toString)
                 .reduce("", String::concat);
+    }
+
+    private static Integer calcPivotStream(int[] array) {
+        return IntStream.rangeClosed(0, array.length - 1)
+                .mapToObj(i -> array.length - 1 - i)
+                .takeWhile(i -> i > 0)
+                .filter(i -> array[i - 1] >= array[i])
+                .map(i -> i-1)
+                .reduce((a, b) -> b).orElse(null);
     }
 
 }
