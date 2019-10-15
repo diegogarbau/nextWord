dd3b3746e8aaf74c90e5cd0ab05f2d3e2550bc62
UW49AT <diego.garcia.bautista.ext@ing.com>
2019-10-15 15:18:28 +0200

    final refactor

--- src/main/java/chapter/LexicographicalOrderMvpImplDGarbau.java
+++ src/main/java/chapter/LexicographicalOrderMvpImplDGarbau.java
@@ -4,25 +4,53 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public class LexicographicalOrderMvpImplDGarbau implements LexicographicalOrder {
+
     @Override
     public Optional<String> nextLargerWord(String word) {
         int[] array = word.chars().toArray();
-        int i = array.length - 1;
-        while (i > 0 && array[i - 1] >= array[i])
-            i--;
-        if (i <= 0) return Optional.empty();
-        int j = array.length - 1;
-        while (array[j] <= array[i - 1])
-            j--;
-        swap(array, i - 1, j);
+        int i = findPivot(array).orElse(0);
+        if (i <= 0) {
+            return Optional.empty();
+        }
+        findSuccessor(array, i);
+        reverseSuffix(array, i);
+        return Optional.of(getStringFromASCIIArray(array));
+    }
 
-        j = array.length - 1;
+    private Optional<Integer> findPivot(int[] array) {
+        int i = array.length - 1;
+        while (i > 0 && array[i - 1] >= array[i]) {
+            i--;
+        }
+        return (i <= 0) ? Optional.empty() : Optional.of(i);
+//        ALTERNATIVE STREAM VERSION
+//        return IntStream.range(0, array.length - 1)
+//            .mapToObj(i -> array.length - 1 - i)
+//            .filter(i -> array[i - 1] < array[i])
+//            .findFirst();
+    }
+
+    private void findSuccessor(int[] array, int i) {
+        int j = array.length - 1;
+        while (array[j] <= array[i - 1]) {
+            j--;
+        }
+        swap(array, i - 1, j);
+//        ALTERNATIVE STREAM VERSION
+//        IntStream.range(0, array.length - 1)
+//            .mapToObj(j -> array.length - 1 - j)
+//            .filter(j -> array[j] > array[i - 1])
+//            .limit(1)
+//            .forEach(j -> swap(array, i - 1, j));
+    }
+
+    private void reverseSuffix(int[] array, int i) {
+        int j = array.length - 1;
         while (i < j) {
             swap(array, i, j);
             i++;
             j--;
         }
-        return Optional.of(getStringFromASCIIArray(array));
     }
 
     private void swap(int[] array, int i, int j) {
@@ -34,9 +62,9 @@ public class LexicographicalOrderMvpImplDGarbau implements LexicographicalOrder 
 
     private String getStringFromASCIIArray(int[] array) {
         return Arrays.stream(array)
-                .boxed()
-                .map(k -> (char) k.intValue())
-                .map(Object::toString)
-                .reduce("", String::concat);
+            .boxed()
+            .map(k -> (char) k.intValue())
+            .map(Object::toString)
+            .reduce("", String::concat);
     }
 }
